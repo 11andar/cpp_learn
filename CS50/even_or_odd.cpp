136127e4b31b4689ec31c0fc47b29e962806853b
Andrzej <darowski@andrzej.com>
2022-12-11 17:54:49 +0100

    Even or odd

--- CS50/even_or_odd.cpp
+++ CS50/even_or_odd.cpp
@@ -3,15 +3,22 @@ using namespace std;
 
 int main(){
     // Get number from user
+    int number;
+    cout << "Number: ";
+    cin >> number;
 
     // Check if number is even
-        
+    if(number % 2 == 0){
         // Output "Even"
+        cout << "Even" << endl;
+    }    
     
     // Check if number is odd
-        
+    if(number % 2 == 0){
         // Output "Odd"
+        cout << "Even" << endl;
+    }
 
     return 0;
-    
+
 }
