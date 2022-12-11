3193a3fc71e719a43772fe6d5591395a1bda8638
Andrzej <darowski@andrzej.com>
2022-12-11 17:51:56 +0100

    TODO list

---
+++ CS50/even_or_odd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Get number from user
+
+    // Check if number is even
+        
+        // Output "Even"
+    
+    // Check if number is odd
+        
+        // Output "Odd"
+
+    return 0;
+    
+}
