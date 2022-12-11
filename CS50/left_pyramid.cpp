fb7137766f98664a74750711003f6a3ddbccbd83
Andrzej <darowski@andrzej.com>
2022-12-11 18:20:55 +0100

    Mario left pyramid

---
+++ CS50/left_pyramid.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int height;
+    
+    do{
+        // Ask user how high he wants the pyramid to be
+        cout << "Height: ";
+        cin >> height; 
+
+    }while(height < 2 || height > 8);
+
+    cout << endl;
+
+    // If 2 <= height <= 8 print the pyramid
+    if(2 <= height <= 8){
+        for(int i = 0; i < height; i++){
+            // For each verese
+            for(int j = 0; j <= i ; j++){
+                cout << "#";
+            }
+            // Go to next verse
+            cout << endl;
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
